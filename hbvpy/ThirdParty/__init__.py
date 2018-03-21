61d3bb9382d207d24ffc8c59ba4182a7141a815e
Marc Girons Lopez <GironsLopez@users.noreply.github.com>
2018-03-21 16:23:40 +0100

    Create __init__.py

---
+++ hbvpy/ThirdParty/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+HBVpy.ThirdParty
+================
+
+Third party classes and functions needed for HBVpy.
+
+"""
+
+from . import core
+from .core import *
+
+__all__ = ['core']
+__all__.extend(core.__all__)
